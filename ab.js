b75895aa0e69810a6916969089ee2bfcc4a82408
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2021-05-17 02:28:42 +0530

    Initial Commit

---
+++ ab.js
@@ -0,0 +1,19 @@
+// obj ={
+//     name:"Ben",
+// }
+// Object.prototype.surname="Parker"
+// obj2={
+// }
+// // console.log(obj2.surname)  //Parker
+// obj3={}
+// console.log(obj3.surname)
+
+
+
+// obj1={
+//     name:"BEN"
+// }
+// obj2=Object.create(obj1);
+// console.log(obj2);
+// console.log(obj2.name);
+
---
+++ index.js
@@ -0,0 +1,9 @@
+a =[2,100];
+obj =Object.assign(a);
+console.log(obj);
+arr=[10,30,40];
+obj2=Object.assign(arr,a);
+console.log(obj2);
+console.log(typeof(obj2))
+console.log(obj2[0]);
+// console.log(obj2.0);
