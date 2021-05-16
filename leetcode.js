e657b85187fea0c8c47b47254d9f93975ffa868d
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2021-05-17 04:21:08 +0530

    Leet-Code Q1 Solve

--- leetcode.js
+++ leetcode.js
@@ -1,2 +1,14 @@
-arr = [4,5,8,6];
-target=10;
+// leetCode Solution Problem1
+arr = [4, 5, 8, 6];
+target = 9;
+function getres(arr,t) {
+    res = [];
+    for (i = 0; i < arr.length; i++) {
+        temp = t - arr[i];
+        if (arr.indexOf(temp) !== -1) {
+            res.push(arr[i], arr[arr.indexOf(temp)])
+            return res;
+        }
+    }
+}
+console.log(getres(arr,target))
---
+++ object.js
@@ -0,0 +1,4 @@
+arr = [0,1,2,3,4,5,6];
+arr2 = [100,101,102,103,104];
+obj=Object.assign(arr,arr2);
+console.log(obj);
