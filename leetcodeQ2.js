78477e94633a4d95e529598ba20b5ba1746b088c
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2021-05-20 01:22:43 +0530

    Leet Code Q2 Wrong - Solution

--- leetcodeQ2.js
+++ leetcodeQ2.js
@@ -1,29 +1,29 @@
-l1 = [2,4,3];
-l2 = [5,6,4];
-// // l1 = [9,9,9,9,9,9,9]
-// // l2 = [9,9,9,9]
-// res=[]
-// l1=l1.reverse().toString().split(',').join('')
-// l2=l2.reverse().toString().split(',').join('')
-// ans=parseInt(l1)+parseInt(l2);
-// ans=ans.toString()
-// for (n of ans){
-//      res.push(parseInt(n))
-// }
-// console.log(res.reverse());
+// l1 = [2,4,3];
+// l2 = [5,6,4];
+// // // l1 = [9,9,9,9,9,9,9]
+// // // l2 = [9,9,9,9]
+// // res=[]
+// // l1=l1.reverse().toString().split(',').join('')
+// // l2=l2.reverse().toString().split(',').join('')
+// // ans=parseInt(l1)+parseInt(l2);
+// // ans=ans.toString()
+// // for (n of ans){
+// //      res.push(parseInt(n))
+// // }
+// // console.log(res.reverse());
 
-var addTwoNumbers = function(l1, l2) {
-     const reducer = (acc,cru)=>acc+cru;
- res=[]
- l1=l1.reverse().toString().split(',').join('')
- l2=l2.reverse().toString().split(',').join('')
- ans=parseInt(l1)+parseInt(l2);
- ans=ans.toString()
- for (n of ans){
-      res.push(parseInt(n))
- }
- return res.reverse();
+// var addTwoNumbers = function(l1, l2) {
+//      const reducer = (acc,cru)=>acc+cru;
+//  res=[]
+//  l1=l1.reverse().toString().split(',').join('')
+//  l2=l2.reverse().toString().split(',').join('')
+//  ans=parseInt(l1)+parseInt(l2);
+//  ans=ans.toString()
+//  for (n of ans){
+//       res.push(parseInt(n))
+//  }
+//  return res.reverse();
 
- }
+//  }
 
- console.log(addTwoNumbers(l1,l2));
+//  console.log(addTwoNumbers(l1,l2));
